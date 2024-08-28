f95f5c3dac98f570ea1ece6e4135e2a164b95772
Harshit Batra <hb99960@gmail.com>
2024-08-28 13:42:17 +0530

    Spring Security Config

---
+++ src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.project.repository;
+
+import com.ecommerce.project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
+}
