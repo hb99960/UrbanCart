08b6b7a4f9031fd284e3808bbbea90060456a44a
Harshit Batra <hb99960@gmail.com>
2024-09-03 11:37:57 +0530

    PostgreSQL configured

--- src/test/java/com/ecommerce/project/repository/CategoryRepositoryTest.java
+++ src/test/java/com/ecommerce/project/repository/CategoryRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CategoryRepositoryTest {
 
     @Autowired
@@ -37,9 +39,13 @@ public class CategoryRepositoryTest {
         categoryRepository.deleteAll();
     }
 
+    // success testcase
     @Test
     void testFindByCategoryNameFound(){
         Category categoryFromDb = categoryRepository.findByCategoryName("Luggage");
         assertThat(categoryFromDb.getCategoryName()).isEqualTo(category.getCategoryName());
     }
+
+    // failure testcase
+
 }
